bf4dd0a3667dd273f1484f09c65e71dd5cebc44f
lenilsondc <lenilson.decastro@yahoo.com>
2020-06-02 22:02:15 -0300

    feat(server): add list all items route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  res.send("Hello World");
+routes.get("/items", async (_, res) => {
+  const items = await knex("items").select("*");
+
+  const mapedItems = items.map((item) => ({
+    id: item.id,
+    title: item.title,
+    image_url: `http://localhost:3333/assets/${item.image}`,
+  }));
+
+  return res.json(mapedItems);
 });
 
 export default routes;
