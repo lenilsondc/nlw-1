27229157009d32fa9f4bca152f1c040106ce9003
lenilsondc <lenilson.decastro@yahoo.com>
2020-06-02 23:16:26 -0300

    feat(server): add show point route

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -3,6 +3,23 @@ import { Request, Response } from "express";
 import knex from "../database/connection";
 
 class PointsController {
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const point = await knex("points").where("id", id).first();
+
+    if (!point) {
+      return res.status(404).json({ message: `Point (id = ${id}) no found` });
+    }
+
+    const items = await knex("items")
+      .join("point_items", "items.id", "=", "point_items.item_id")
+      .where("point_items.id", id)
+      .select("title");
+
+    return res.json({ ...point, items });
+  }
+
   async create(req: Request, res: Response) {
     const {
       name,
