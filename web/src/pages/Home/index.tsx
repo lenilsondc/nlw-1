086528a2007dafc9f8e571d38935fae1c48193ac
lenilsondc <lenilson.decastro@yahoo.com>
2020-06-03 22:47:41 -0300

    fix(web): change link to RouterLink

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FiLogIn } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 import "./styles.css";
 import logo from "../../assets/logo.svg";
@@ -15,12 +16,12 @@ const Home: React.FC = () => {
           <h1>Your waste collection marketplace.</h1>
           <p>Helping people find collection points efficiently.</p>
 
-          <a href="/signup">
+          <Link to="/create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>Register a collection point</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
