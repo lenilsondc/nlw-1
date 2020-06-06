37d8b71facba58244a74be5e50b66fff6107ef06
lenilsondc <lenilson.decastro@yahoo.com>
2020-06-05 23:13:05 -0300

    feat(mobile): implement Home features

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,38 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { Feather as Icon } from "@expo/vector-icons";
-import { View, Text, StyleSheet, Image, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
 
 const Home = () => {
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+
   const { navigate } = useNavigation();
 
   function handleNavigateToPoints() {
-    navigate("Points");
+    navigate("Points", { state, city });
   }
 
   return (
-    <ImageBackground
-      style={styles.container}
-      source={require("../../assets/home-background.png")}
-      imageStyle={{ width: 274, height: 368 }}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
     >
-      <View style={styles.main}>
-        <Image source={require("../../assets/logo.png")} />
-        <Text style={styles.title}>Your waste collection market</Text>
-        <Text style={styles.description}>
-          We help people find collection points efficiently.
-        </Text>
-      </View>
-      <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
-          <View style={styles.buttonIcon}>
-            <Icon name="arrow-right" color="#fff" size={24} />
+      <ImageBackground
+        style={styles.container}
+        source={require("../../assets/home-background.png")}
+        imageStyle={{ width: 274, height: 368 }}
+      >
+        <View style={styles.main}>
+          <Image source={require("../../assets/logo.png")} />
+          <View>
+            <Text style={styles.title}>Your waste collection market</Text>
+            <Text style={styles.description}>
+              We help people find collection points efficiently.
+            </Text>
           </View>
-          <Text style={styles.buttonText}>Find</Text>
-        </RectButton>
-      </View>
-    </ImageBackground>
+        </View>
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Select a state"
+            autoCapitalize="characters"
+            maxLength={2}
+            autoCorrect={false}
+            value={state}
+            onChangeText={setState}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Select a City"
+            value={city}
+            onChangeText={setCity}
+            autoCorrect={false}
+          />
+
+          <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+            <View style={styles.buttonIcon}>
+              <Icon name="arrow-right" color="#fff" size={24} />
+            </View>
+            <Text style={styles.buttonText}>Find</Text>
+          </RectButton>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
