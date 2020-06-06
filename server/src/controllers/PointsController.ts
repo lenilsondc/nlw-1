0a8635e99bed17c053edb57845c7895fd91cec02
lenilsondc <lenilson.decastro@yahoo.com>
2020-06-05 21:26:49 -0300

    fix(server): fix knex query join

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -11,7 +11,7 @@ class PointsController {
       .map((item) => Number(item.trim()));
 
     const points = await knex("points")
-      .join("point_items", "points.id", "=", "point_items.item_id")
+      .join("point_items", "points.id", "=", "point_items.point_id")
       .whereIn("point_items.item_id", parsedItems)
       .where("city", String(city))
       .where("state", String(state))
