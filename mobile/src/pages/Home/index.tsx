fd650ce476e77d2f063953b42f8b382bee7a5c63
lenilsondc <lenilson.decastro@yahoo.com>
2020-06-05 23:08:58 -0300

    feat(mobile): setup main routes

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -2,30 +2,39 @@ import React from "react";
 import { Feather as Icon } from "@expo/vector-icons";
 import { View, Text, StyleSheet, Image, ImageBackground } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
-const Home = () => (
-  <ImageBackground
-    style={styles.container}
-    source={require("../../assets/home-background.png")}
-    imageStyle={{ width: 274, height: 368 }}
-  >
-    <View style={styles.main}>
-      <Image source={require("../../assets/logo.png")} />
-      <Text style={styles.title}>Your waste collection market</Text>
-      <Text style={styles.description}>
-        We help people find collection points efficiently.
-      </Text>
-    </View>
-    <View style={styles.footer}>
-      <RectButton style={styles.button} onPress={() => {}}>
-        <View style={styles.buttonIcon}>
-          <Icon name="arrow-right" color="#fff" size={24} />
-        </View>
-        <Text style={styles.buttonText}>Find</Text>
-      </RectButton>
-    </View>
-  </ImageBackground>
-);
+const Home = () => {
+  const { navigate } = useNavigation();
+
+  function handleNavigateToPoints() {
+    navigate("Points");
+  }
+
+  return (
+    <ImageBackground
+      style={styles.container}
+      source={require("../../assets/home-background.png")}
+      imageStyle={{ width: 274, height: 368 }}
+    >
+      <View style={styles.main}>
+        <Image source={require("../../assets/logo.png")} />
+        <Text style={styles.title}>Your waste collection market</Text>
+        <Text style={styles.description}>
+          We help people find collection points efficiently.
+        </Text>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+          <View style={styles.buttonIcon}>
+            <Icon name="arrow-right" color="#fff" size={24} />
+          </View>
+          <Text style={styles.buttonText}>Find</Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
+  );
+};
 
 export default Home;
 
@@ -33,7 +42,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    backgroundColor: "#f0f0f5",
   },
 
   main: {
